21edc224568d1429bcc0c4f14cee8fda12be1f95
Märt Konsap <mrt.konsap@gmail.com>
2022-08-05 12:48:48 +0300

    Refactor and remove favicon requests

--- src/main/kotlin/eu/m2rt/reader/Main.kt
+++ src/main/kotlin/eu/m2rt/reader/Main.kt
@@ -2,10 +2,7 @@ package eu.m2rt.reader
 
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
-import io.javalin.http.Context
-import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
-import java.net.URL
 
 
 fun main(vararg args: String) {
@@ -27,61 +24,14 @@ fun main(vararg args: String) {
         }
     }
 
-    WanderingInnProxy(
-        app,
-        getResourceAsText("/wandering-inn-wrapper.html"),
-    )
+    WanderingInnProxy(getResourceAsText("/wandering-inn-wrapper.html")).registerRoutes(app)
 
     val index = getResourceAsText("/index.html")
 
     app.routes {
-        get("/favicon.ico") { ctx -> ctx.status(404) }
         get { it.resultHtml(index) }
     }
 
     app.start(port)
 }
 
-class WanderingInnProxy(
-    app: Javalin,
-    private val wrapperHtml: String
-) {
-
-    init {
-        app.routes {
-            get("/wandering-inn") {
-                it.renderWanderingInn("https://wanderinginn.com/table-of-contents/")
-            }
-
-            get("/wandering-inn/*") {
-                it.renderWanderingInn("https://wanderinginn.com/${it.path().removePrefix("/wandering-inn/")}")
-            }
-        }
-    }
-
-    private fun Context.renderWanderingInn(originalUrl: String) {
-        val document = Jsoup.parse(
-            URL(originalUrl),
-            10_000
-        )
-
-        val content = document.body()
-            .getById("content")
-            .getFirstByTag("article")
-
-        content.select("a[href]").forEach { a ->
-            a.attr(
-                "href",
-                a.attr("href")
-                    .replaceFirst("https://wanderinginn.wordpress.com/", "/wandering-inn/")
-                    .replaceFirst("https://wanderinginn.com/", "/wandering-inn/")
-            )
-        }
-
-        resultHtml(
-            wrapperHtml
-                .replace("\${ORIGINAL_URL}", originalUrl)
-                .replace("\${CONTENT}", content.outerHtml())
-        )
-    }
-}
---
+++ src/main/kotlin/eu/m2rt/reader/WanderingInnProxy.kt
@@ -0,0 +1,50 @@
+package eu.m2rt.reader
+
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder
+import io.javalin.http.Context
+import org.jsoup.Jsoup
+import java.net.URL
+
+class WanderingInnProxy(
+    private val wrapperHtml: String
+) {
+
+    fun registerRoutes(app: Javalin) {
+        app.routes {
+            ApiBuilder.get("/wandering-inn") {
+                it.renderWanderingInn("https://wanderinginn.com/table-of-contents/")
+            }
+
+            ApiBuilder.get("/wandering-inn/*") {
+                it.renderWanderingInn("https://wanderinginn.com/${it.path().removePrefix("/wandering-inn/")}")
+            }
+        }
+    }
+
+    private fun Context.renderWanderingInn(originalUrl: String) {
+        val document = Jsoup.parse(
+            URL(originalUrl),
+            10_000
+        )
+
+        val content = document.body()
+            .getById("content")
+            .getFirstByTag("article")
+
+        content.select("a[href]").forEach { a ->
+            a.attr(
+                "href",
+                a.attr("href")
+                    .replaceFirst("https://wanderinginn.wordpress.com/", "/wandering-inn/")
+                    .replaceFirst("https://wanderinginn.com/", "/wandering-inn/")
+            )
+        }
+
+        resultHtml(
+            wrapperHtml
+                .replace("\${ORIGINAL_URL}", originalUrl)
+                .replace("\${CONTENT}", content.outerHtml())
+        )
+    }
+}
