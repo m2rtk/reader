fe238f2a193c269054daf73384f0afef401df0e2
Märt Konsap <mrt.konsap@gmail.com>
2022-08-05 19:15:06 +0300

    Add stylesheet select support

--- src/main/kotlin/eu/m2rt/reader/Main.kt
+++ src/main/kotlin/eu/m2rt/reader/Main.kt
@@ -2,8 +2,14 @@ package eu.m2rt.reader
 
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.post
+import io.javalin.http.Cookie
+import io.javalin.http.staticfiles.Location
 import org.slf4j.LoggerFactory
+import java.util.*
 
+val CSS_OPTIONS = listOf("dark", "default")
+val CSS = CSS_OPTIONS.associateWith { getResourceAsText("/assets/$it.css") }
 
 fun main(vararg args: String) {
     val port = args.firstOrNull()?.toInt() ?: 8080
@@ -11,6 +17,12 @@ fun main(vararg args: String) {
     val app: Javalin = Javalin.create {
         it.showJavalinBanner = false
         it.enableCorsForAllOrigins()
+
+        it.addStaticFiles { staticFiles ->
+            staticFiles.hostedPath = "/"
+            staticFiles.directory = "/assets"
+            staticFiles.location = Location.CLASSPATH
+        }
 
         it.requestLogger { ctx, executionTimeMs ->
             log.info(
@@ -28,8 +40,37 @@ fun main(vararg args: String) {
 
     val index = getResourceAsText("/index.html")
 
+    app.before {
+        val style = it.cookie("style")
+
+        if (style == null) {
+            it.cookie(Cookie("style", "default"))
+        }
+
+        val id = it.cookie("reader")
+
+        if (id == null) {
+            it.cookie(Cookie("reader", UUID.randomUUID().toString()))
+        }
+    }
+
     app.routes {
         get { it.resultHtml(index) }
+        get("/style.css") {
+            val style = it.cookie("style") ?: "default"
+            it.result(CSS[style] ?: CSS["default"]!!)
+        }
+        post("/style") {
+            val cookie = it.cookie("style")
+            val newStyleIndex = cookie?.let { c -> CSS_OPTIONS.indexOf(c) + 1 } ?: 0
+
+            if (newStyleIndex == 0 || newStyleIndex >= CSS_OPTIONS.size) {
+                it.cookie(Cookie("style", CSS_OPTIONS[0]))
+            } else {
+                it.cookie(Cookie("style", CSS_OPTIONS[newStyleIndex]))
+            }
+        }
+
     }
 
     app.start(port)
